806ffb6ea92f82c6a4ab401586deb93c28a2cc27
songshilian <songshilian@qiangungun.com>
2022-10-13 22:40:28 +0800

    项目初始化

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ShowCom from './view/Show';
+
+function App() {
+  return (
+    <div className="App">
+      <ShowCom></ShowCom>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/view/Show.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../css/show.css';
+
+class Show extends Component {
+  render() {
+    return (
+      <div className='show-wrap' >
+        <div></div>
+        <div>
+            <h1>瘋傳！60歲阿嬤炒股三年盈利超仟萬靠這1招獲利至今！</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Show;
